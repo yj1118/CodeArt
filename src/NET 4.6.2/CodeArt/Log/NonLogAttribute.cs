69201f8a1c1bf2fe3b56ac39588a5505a8c7b5e3
yuanjun <yuanjun1118@gmail.com>
2017-07-09 16:58:48 +0800

    Signed-off-by: yuanjun <yuanjun1118@gmail.com>

---
+++ src/NET 4.6.2/CodeArt/Log/NonLogAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace CodeArt.Log
+{
+    /// <summary>
+    /// 标示对象信息不记录日志
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = true)]
+    public class NonLogAttribute : Attribute
+    {
+        public NonLogAttribute() { }
+
+        #region 辅助
+
+        /// <summary>
+        /// 获取类型定义的<see cref="NonLogAttribute"/>信息
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static bool IsDefined(Type type)
+        {
+            return type.GetCustomAttributes(typeof(NonLogAttribute), true).Length > 0;
+        }
+
+        public static bool IsDefined(Exception ex)
+        {
+            if (IsDefined(ex.GetType())) return true;
+            if (ex.Data.Contains(MarkecCode)) return true;
+            return false;
+        }
+
+        public static void Define(Exception ex)
+        {
+            ex.Data.Add(NonLogAttribute.MarkecCode, true);
+        }
+
+        #endregion
+
+        public const string MarkecCode = "NonLog";
+
+    }
+}
---
+++ src/NET 4.6/CodeArt/Log/LogWrapper.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NLog;
+using CodeArt.Util;
+using CodeArt.Concurrent;
+
+namespace CodeArt.Log
+{
+    /// <summary>
+    /// 该对象是线程安全的
+    /// </summary>
+    public class LogWrapper
+    {
+        Pool<Logger> _pool = null;
+
+        public LogWrapper(Func<Logger> createLogger)
+        {
+            _pool = new Pool<Logger>(() =>
+            {
+                return createLogger();
+            }, (log, phase) =>
+            {
+                return true;
+            }, new PoolConfig()
+            {
+                LoanCapacity = 20
+            });
+        }
+
+        /// <summary>
+        /// 写入致命错误的消息
+        /// </summary>
+        /// <param name="message"></param>
+        public void Fatal(string message)
+        {
+            using(var item = _pool.Borrow())
+            {
+                var log = item.Item;
+                log.Fatal(message);
+            }
+        }
+
+        public void Fatal(Exception ex)
+        {
+            if (NonLogAttribute.IsDefined(ex)) return;
+            var tracker = ex as ITracker;
+
+            if (tracker == null)
+                Fatal(string.Format("消息：{0}；堆栈：{1}", ex.GetCompleteMessage(), ex.GetCompleteStackTrace()));
+            else
+                Fatal(string.Format("消息：{0}；堆栈：{1}", tracker.GetMessage(), ex.GetCompleteStackTrace()));
+        }
+
+        /// <summary>
+        /// 写入调试的消息
+        /// </summary>
+        /// <param name="message"></param>
+        public void Debug(string message)
+        {
+            using (var item = _pool.Borrow())
+            {
+                var log = item.Item;
+                log.Debug(message);
+            }
+        }
+
+        /// <summary>
+        /// 写入跟踪的消息
+        /// </summary>
+        /// <param name="message"></param>
+        public void Trace(string message)
+        {
+            using (var item = _pool.Borrow())
+            {
+                var log = item.Item;
+                log.Trace(message);
+            }
+        }
+
+        public void Info(string message)
+        {
+            using (var item = _pool.Borrow())
+            {
+                var log = item.Item;
+                log.Info(message);
+            }
+        }
+
+        public void Write(LogEventInfo ei)
+        {
+            using (var item = _pool.Borrow())
+            {
+                var log = item.Item;
+                log.Log(ei);
+            }
+        }
+
+        #region 静态成员
+
+        public static readonly LogWrapper Default = new LogWrapper(() =>
+        {
+            return LogManager.GetCurrentClassLogger();
+        });
+
+        #endregion
+    }
+}
